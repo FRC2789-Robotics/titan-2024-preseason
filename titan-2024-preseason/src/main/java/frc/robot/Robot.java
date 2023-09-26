16bb7ebb10e0feaaf3b07b484b5cf9aecbd99f42
pfishdome <pfishdome2@gmail.com>
2023-09-26 17:32:11 -0500

    initial commit

---
+++ titan-2024-preseason/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+
+
+  @Override
+  public void robotInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    
+  }
+}
